ccfca9ee2d88ce3204938560a13cfce98132f775
omadbekolimov <olimovomadbek96@gmail.com>
2024-04-29 20:35:01 +0500

    Js start

---
+++ js/lesson1.js
@@ -0,0 +1,30 @@
+// let son = prompt("Ism kiriting :")
+// alert(son)
+  // let var const 
+  
+  var a = 10 // var o'zgaruvchi turi  a -> o'zgaruvchi nomi  10 -> o'zgaruvchining qiymati 
+//   console.log(a);
+//    a=12 
+//   console.log(a);
+
+//   let son = 15
+//   console.log(son);
+
+//   son = 17 
+//   console.log(son);
+
+//   const name = "Omadbek"
+//   console.log(name);
+
+//   name = "Ozodbek"
+//   console.log(name);
+
+//  ism familiya  yosh manzil  yosh number 
+  
+   let ism =prompt("Ism kiriting :")
+   let familiya =prompt("Familiya kiriting")
+//    alert(`Sizning ismingiz ${ism} Sizning familiyangiz ${familiya}`)
+   alert("Sizning ismingiz "  + ism + " Sizning familiyangiz " + familiya)
+
+ 
+
